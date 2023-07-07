f425c9381a19d5dad9e21f8fa76f71bfd3c5e87e
Joazen <johnsons544@gmail.com>
2023-07-08 00:21:52 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class binarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    }else{
+        do{
+            
+        }while()
+    }
+  }
+
+  lookup(value) {}
+}
